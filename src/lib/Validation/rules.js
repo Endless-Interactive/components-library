9fbe8814d86878b2f4f26363001057f8b0975746
Walrusking <walruskinggaming@gmail.com>
2023-03-13 22:51:55 -0400

    fixes packaging error

--- src/lib/Validation/rules.js
+++ src/lib/Validation/rules.js
@@ -1,64 +1,42 @@
-/**
- * @param {any} value
- */
 export function required(value) {
-  const passed = typeof value === 'boolean' ? value : value.length > 0;
-  return { passed, reason: 'This field is required' };
+  const passed = typeof value === "boolean" ? value : value.length > 0;
+  return { passed, reason: "This field is required" };
 }
 
-/**
- * @param {number} minLength
- */
 export function minLength(minLength) {
   return function(/** @type {any} */ value) {
     const passed = value.length >= minLength;
     return { passed, reason: `This field must be at least ${minLength} characters` };
-  }
+  };
 }
 
-/**
- * @param {number} maxLength
- * @returns {function(*): {reason: string, passed: boolean}}
- */
 export function maxLength(maxLength) {
   return function(/** @type {any} */ value) {
     const passed = value.length <= maxLength;
     return { passed, reason: `This field must be at most ${maxLength} characters` };
-  }
+  };
 }
 
-/**
- * @param {any} value
- */
 export function email(value) {
   const passed = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value);
-  return { passed, reason: 'This field must be a valid email' };
+  return { passed, reason: "This field must be a valid email" };
 }
 
-/**
- * @param {any} value
- */
 export function number(value) {
   const passed = !isNaN(value);
-  return { passed, reason: 'This field must be a number' };
+  return { passed, reason: "This field must be a number" };
 }
 
-/**
- * @param {any} min
- */
 export function min(min) {
   return function(/** @type {any} */ value) {
     const passed = value >= min;
     return { passed, reason: `This field must be at least ${min}` };
-  }
+  };
 }
 
-/**
- * @param {any} max
- */
 export function max(max) {
   return function(/** @type {any} */ value) {
     const passed = value <= max;
     return { passed, reason: `This field must be at most ${max}` };
-  }
+  };
 }
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
-export { default as Timestamp } from './Components/Timestamp.svelte';
-export { default as Form } from './Components/Form.svelte';
-export { validate, validateForm } from './Validation';
+export { default as Timestamp } from "./Components/Timestamp.svelte";
+export { default as Form } from "./Components/Form.svelte";
+export { validate, validateForm } from "./Validation";
+export { required, email, minLength, maxLength, min, max, number } from "./Validation/rules";
